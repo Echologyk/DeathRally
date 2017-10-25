a5fc7561086868b2c149dd0d135ae0c8321dbb8d
Jules <jules.briquet@ubisoft.com>
2017-10-25 14:15:00 +0200

    Project Initialization

---
+++ Assets/Scripts/CarMovementComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovementComponent : MonoBehaviour {
+
+    float CurrentSpeed = 0f;
+
+    public float MaxSpeed = 5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        CurrentSpeed = MaxSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (CurrentSpeed > 0f)
+        {
+            Vector3 Velocity = new Vector3(0, 0, CurrentSpeed);
+            transform.position += Velocity;
+        }
+	}
+
+    public float GetCurrentSpeed()
+    {
+        return CurrentSpeed;
+    }
+}
