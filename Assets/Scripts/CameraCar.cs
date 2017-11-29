130fc13684eec5dd984f5c66ccee84713f3ec539
Unknown <loic.guilbaud3@gmail.com>
2017-11-29 18:59:02 +0100

    Add 3D Car + Attach Camera to it
    Add 3D file from Store+ Add a Camera above the car

--- Assets/Scripts/CameraCar.cs
+++ Assets/Scripts/CameraCar.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class CameraCar : MonoBehaviour {
 
+	public CarMovement CarToFollow; 
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
 }
